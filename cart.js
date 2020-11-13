057692d09d85177aa41ae4a4b71d880d72bdb91f
chathuri <ckthambugala@gmail.com>
2020-11-13 10:09:36 +0530

    this is the shopping  cart web site

---
+++ cart.js
@@ -0,0 +1,32 @@
+//varibles
+const cartBtn= document.querySelector(".cartbtn");
+const addcart= document.querySelector(".btn");
+const clearcart= document.querySelector(".clearcart");
+const cartItems= document.querySelector(".col4");
+const cartTot= document.querySelector(".carttot");
+const cartContent= document.querySelector(".cartinfo");
+const productsDom= document.querySelector(".row");
+
+//cart
+let cart=[];
+
+//getting products
+class product{
+
+}
+
+//display products
+
+class UI{
+
+}
+
+//Local Storage
+class storage{
+
+}
+
+document.addEventListener("DOMContentLoader",()=>{
+const ui=new UI();
+const Product=new product();
+})
