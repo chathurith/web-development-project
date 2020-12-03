fd5c8e6153957a449e87194a1987e35505300ff9
chathuri <ckthambugala@gmail.com>
2020-12-04 00:13:11 +0530

    developing

--- shoppingCart.js
+++ shoppingCart.js
@@ -31,3 +31,55 @@ function onLoadCartNumbers() {
     document.querySelector(".cart span").textContent = productNumbers;
   }
 }
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem("cartNumbers");
+  
+    productNumbers = parseInt(productNumbers);
+  
+    if (productNumbers) {
+      localStorage.setItem("cartNumbers", productNumbers + 1);
+      document.querySelector(".cart span").textContent = productNumbers + 1;
+    } else {
+      localStorage.setItem("cartNumbers", 1);
+      document.querySelector(".cart span").textContent = 1;
+    }
+  
+    setItems(product);
+  }
+
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.id] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.id]: product,
+      };
+    }
+
+    cartItems[product.id].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = { [product.id]: product };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem("totalCost");
+    console.log("cost", cartCost);
+  
+    if (cartCost != null) {
+      cartCost = parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+      localStorage.setItem("totalCost", product.price);
+    }
+  }
+
+
+  
